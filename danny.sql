d9542134786fcc371d9c50f9efb57a6e140d9c54
Boluwatife <116385978+fedan22@users.noreply.github.com>
2023-06-11 22:06:45 +0100

    Add files via upload

---
+++ danny.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 11, 2023 at 11:03 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `danny`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `members`
+--
+
+CREATE TABLE `members` (
+  `customer_id` varchar(1) DEFAULT NULL,
+  `join_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `members`
+--
+
+INSERT INTO `members` (`customer_id`, `join_date`) VALUES
+('A', '2021-01-07'),
+('B', '2021-01-09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `product_id` int(11) DEFAULT NULL,
+  `product_name` varchar(10) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`product_id`, `product_name`, `price`) VALUES
+(1, 'sushi', 10),
+(2, 'curry', 15),
+(3, 'ramen', 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `customer_id` varchar(1) DEFAULT NULL,
+  `order_date` date DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`customer_id`, `order_date`, `product_id`) VALUES
+('A', '2021-01-01', 1),
+('A', '2021-01-01', 2),
+('A', '2021-01-07', 2),
+('A', '2021-01-10', 3),
+('A', '2021-01-11', 3),
+('A', '2021-01-11', 3),
+('B', '2021-01-01', 2),
+('B', '2021-01-02', 2),
+('B', '2021-01-04', 1),
+('B', '2021-01-11', 1),
+('B', '2021-01-16', 3),
+('B', '2021-02-01', 3),
+('C', '2021-01-01', 3),
+('C', '2021-01-01', 3),
+('C', '2021-01-07', 3);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
